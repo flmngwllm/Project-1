0232a616eabb2bdc092ebf539f9edfdf5d3281ea
CB_ATL_117_William_Fleming <cb_atl_117_william_fleming@CBs-MacBook-Air.local>
2018-10-12 11:40:14 -0400

    added visible columns and rows to help

--- app.js
+++ app.js
@@ -2,12 +2,33 @@ var canvas = document.querySelector('canvas')
 
 var scHeight = 600
 var scWidth = 300
+var Columns = 10
+var Rows = 20
+var emp = 'white'
 
 canvas.width = scWidth
 canvas.height = scHeight
 
 var c = canvas.getContext("2d")
 c.fillStyle = 'black'
+
+
+let field = []
+for( h = 0;  h < Rows; h++){
+    field[h] = []
+    for (v = 0; v < Columns; v++){
+        field[h][v] = emp
+    } 
+}
+
+function drawfield(){
+    for(h = 0; h < Rows; h++){
+        for(v = 0; v < Columns; v++){
+            
+        }
+    }
+}
+
 
 const lBlock =[ [0, 1, 0]
                [0, 1, 0]
@@ -18,20 +39,20 @@ const tBlock = [[0, 1, 0]
                [0, 0, 0]
 ]
 const sBlock= [[0, 1, 1]
-              [0, 1, 0]
-              [1, 1, 0]
+               [0, 1, 0]
+               [1, 1, 0]
 ]
 const zBlock=  [[1, 1, 0]
                 [0, 1, 0]
                 [0, 1, 1]
 ]
-const squareBlock = [[0, 1, 0]
-                    [0, 1, 0]
-                    [0, 1, 1]
+const squareBlock =[[1, 1]
+                    [1, 1]
+                    
 ]
 const blBlock = [[0, 1, 0]
-                [0, 1, 0]
-                [1, 1, 1]
+                 [0, 1, 0]
+                 [1, 1, 0]
 ]
 const iBlock = [[0, 1, 0]
                 [0, 1, 0]
@@ -43,6 +64,14 @@ var blocks = [lBlock, sBlock, zBlock, tBlock, blBlock, squareBlock, iBlock]
 
 var colors = ['blue', 'green', 'yellow', 'red', 'orange', 'pink', 'purple']
 
+
+ranColor = colors[Math.floor(Math.random() * colors.length)]
+
+ranBlock = blocks[Math.floor(Math.random() * blocks.length)]
+
+
+
+document.addEventlistner("keydown", controller)
 
 
 
