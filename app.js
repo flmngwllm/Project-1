75c85084c5b7279e783ca29277b092b0e64095e0
CB_ATL_117_William_Fleming <cb_atl_117_william_fleming@CBs-MacBook-Air.local>
2018-10-13 22:09:05 -0400

    arrays were wrong fixing

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const Rows = 20
 const sq = 20
 const emp = '#ffffff'
 
+
+
+// // Controls using keycodes to assign each button as a movement
+//  document.addEventlistner("keydown", controller)
+
+
 //function to colors the board and squares on the canvas
 function showSq(x,y,color){
     c.fillStyle = color
@@ -50,91 +56,130 @@ const tBlock = [[0, 1, 0]
                [1, 1, 1]
                [0, 0, 0]
 ]
-const sBlock= [[0, 1, 1]
-               [1, 1, 0]
-               [0, 0, 0],
-
-               [0, 1, 0]
-               [0, 1, 1]
-               [0, 0, 1],
-
-               [0, 0, 0]
-               [0, 1, 1]
+const sBlock= [
+               [[0, 1, 1],
                [1, 1, 0],
+               [0, 0, 0],
+],
 
-               [1, 0, 0]
-               [1, 1, 0]
-               [0, 1, 0]
+[
+               [0, 1, 0],
+               [0, 1, 1],
+               [0, 0, 1],
+],
+
+[
+               [0, 0, 0],
+               [0, 1, 1],
+               [1, 1, 0],
+],
+
+[
+               [1, 0, 0],
+               [1, 1, 0],
+               [0, 1, 0],
 ]
-const zBlock=  [[1, 1, 0]
-                [0, 1, 1]
+]
+
+
+const zBlock= [ [[1, 1, 0],
+                [0, 1, 1],
                 [0, 0, 0],
+],
 
-                [0, 1, 0]
-                [0, 1, 1]
+[
+                [0, 1, 0],
+                [0, 1, 1],
                 [0, 0, 1],
+],
 
-                [0, 0, 0]
-                [1, 1, 0]
+[
+                [0, 0, 0],
+                [1, 1, 0],
                 [0, 1, 1],
 
-                [0, 1, 0]
-                [1, 1, 0]
-                [1, 0, 0]
+],
+[
+                [0, 1, 0],
+                [1, 1, 0],
+                [1, 0, 0],
 ]
-const squareBlock =[[1, 1]
-                    [1, 1]
+]
+const squareBlock =[[1, 1],
+                    [1, 1],
                     
 ]
-const blBlock = [[0, 1, 0]
-                 [0, 1, 0]
-                 [1, 1, 0]
+const blBlock = [[0, 1, 0],
+                 [0, 1, 0],
+                 [1, 1, 0],
 ]
-const iBlock = [[0, 0, 1, 0]
-                [0, 0, 1, 0]
-                [0, 0, 1, 0]
+
+
+                [[0, 1, 0],
+                 [0, 1, 0],
+                 [1, 1, 0],
+]
+
+
+[[0, 1, 0],
+                 [0, 1, 0],
+                 [1, 1, 0],
+]
+
+
+[[0, 1, 0],
+                 [0, 1, 0],
+                 [1, 1, 0],
+]
+const iBlock = [[0, 0, 1, 0],
+                [0, 0, 1, 0],
+                [0, 0, 1, 0],
                 [0, 0, 1, 0],
 
-                [0, 0, 0, 0]
-                [1, 1, 1, 1]
-                [0, 0, 0, 0]
+                [0, 0, 0, 0],
+                [1, 1, 1, 1],
+                [0, 0, 0, 0],
                 [0, 0, 0, 0],
 
-                [0, 1, 0, 0]
-                [0, 1, 0, 0]
-                [0, 1, 0, 0]
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
                 [0, 1, 0, 0],
 
-                [0, 0, 0, 0]
-                [0, 0, 0, 0]
-                [1, 1, 1, 1]
-                [0, 0, 0, 0]
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [1, 1, 1, 1],
+                [0, 0, 0, 0],
 ]
 
 //saving block to variable as an arry to use later
-var blocks = [lBlock, sBlock, zBlock, tBlock, blBlock, squareBlock, iBlock]
+var blocks = [
+    [lBlock, "red"], [sBlock, "green"], [zBlock, "yellow"], [tBlock, "orange"], [blBlock, "pink"], [squareBlock,"blue"], [iBlock,"purple"]
+]
 // defining and setting colors that will be used in an array for each block that will be used
-var colors = ['blue', 'green', 'yellow', 'red', 'orange', 'pink', 'purple']
+// var colors = ['blue', 'green', 'yellow', 'red', 'orange', 'pink', 'purple']
 
-//randomizing colors using math.random by through the length of the array
-ranColor = colors[Math.floor(Math.random() * colors.length)]
 
 //randomizing the different blocks that will appear
-ranBlock = blocks[Math.floor(Math.random() * blocks.length)]
+function ranBlock(){ 
+    let r = ranBlock = Math.floor(Math.random() * blocks.length)
+    return new createBlocks(blocks[r][0],blocks[r][1]) 
+}
+
+let b = ranBlock()
 
 
-// Controls using keycodes to assign each button as a movement
-document.addEventlistner("keydown", controller)
+
 
 //object for the blocks with color parameters 
-function createBlocks(blocks ,color){
-    this.blocks = blocks
+function createBlocks(type ,color){
+    this.type = type
     this.color = color 
 
     this.shapes = 0
-    this.mobileShape = this.blocks[this.shapes]
+    this.mobileShape = this.type[this.shapes]
 
-    this.y = -2
+    this.y = 3
     this.x =3
 
 }
@@ -142,7 +187,7 @@ function createBlocks(blocks ,color){
 createBlocks.prototype.fill = function(color){
     for(h= 0; h < this.mobileShape.length; h++){
         for(v = 0; v < this.mobileShape.length; v++){
-if(this.mobileShape[v][h]){
+if(this.mobileShape[h][v]){
     showSq(this.x + v, this.y + h, color)
 }
         }
@@ -154,7 +199,12 @@ createBlocks.prototype.draw = function(){
     this.fill(this.color)
 }
 
+b.draw()
+
+
 
 createBlocks.prototype.destroy = function(){
     this.fill(emp)
 }
+
+
